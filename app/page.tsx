0fc7ae499b63cd34b1ba44ed28da674db689af35
James Grieve <jamesonrgrieve@gmail.com>
2025-03-02 10:59:10 -0700

    Move InteractiveHome into its own component;

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-import { CallToAction } from '@/components/interactive/Home/call-to-action';
-import { Contact } from '@/components/interactive/Home/contact';
-import { Features } from '@/components/interactive/Home/features';
-import { Hero } from '@/components/interactive/Home/hero';
-import { HowItWorks } from '@/components/interactive/Home/how-it-works';
-import PricingGrid from '@/components/jrg/auth/stripe/PricingTable';
+import InteractiveHome from '@/components/interactive/Home';
 import { ThemeToggle } from '@/components/jrg/theme/ThemeToggle';
 import { Button } from '@/components/ui/button';
 import { cookies } from 'next/headers';
@@ -36,18 +31,7 @@ export default function Home() {
         </div>
       </header>
       <main>
-        <Hero />
-        <Features />
-        <HowItWorks />
-
-        <CallToAction />
-        <div className='flex flex-col items-center justify-center'>
-          <PricingGrid />
-        </div>
-        <Contact />
-        <div className='flex flex-col items-center justify-center'>
-          <a href='/privacy'>Privacy Policy</a>
-        </div>
+        <InteractiveHome />
       </main>
     </div>
   );
