ef6694f17c7f65ec9377065bc6082d4808c47367
waiscodes <b.wais@outlook.com>
2025-03-11 14:30:04 -0600

    add header to chat and button to sidebar

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,12 +1,37 @@
 'use client';
 
-import { SidebarMain } from '@/appwrapper/SidebarHeader';
+import { SidebarMain, SidebarHeader, SidebarHeaderTitle } from '@/appwrapper/SidebarHeader';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 import { SidebarInset } from '@/components/ui/sidebar';
+import { useSidebar } from '@/components/ui/sidebar';
+import { ViewVerticalIcon } from '@radix-ui/react-icons';
+import { useParams } from 'next/navigation';
 
 export default function ChatLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarInset>
+      <ChatHeader />
       <SidebarMain>{children}</SidebarMain>
     </SidebarInset>
+  );
+}
+
+function ChatHeader() {
+  const { toggleSidebar } = useSidebar('right');
+  const { id } = useParams();
+  // TODO: get conversation name from id
+
+  return (
+    <SidebarHeader>
+      <SidebarHeaderTitle>{id ? 'Chat' : 'New Chat'}</SidebarHeaderTitle>
+      <div className='flex items-center h-full md:hidden'>
+        <Separator orientation='vertical' className='h-4' />
+        <Button variant='ghost' size='icon' onClick={toggleSidebar}>
+          <ViewVerticalIcon />
+          <span className='sr-only'>Toggle Sidebar</span>
+        </Button>
+      </div>
+    </SidebarHeader>
   );
 }
