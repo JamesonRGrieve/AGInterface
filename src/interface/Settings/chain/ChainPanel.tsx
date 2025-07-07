8b0aeeba9c5e1ee4735aec7a9078b71736753f3a
waiscodes <58951863+waiscodes@users.noreply.github.com>
2025-07-07 04:33:21 -0600

    update chain select

--- src/interface/Settings/chain/ChainPanel.tsx
+++ src/interface/Settings/chain/ChainPanel.tsx
@@ -13,103 +13,21 @@ import { Button } from '@/components/ui/button';
 import { LuDownload } from 'react-icons/lu';
 import { Label } from '@/components/ui/label';
 
-export default function ChainPanel({
-  showCreateDialog,
-  setShowCreateDialog,
-}: {
-  showCreateDialog: boolean;
-  setShowCreateDialog: (show: boolean) => void;
-}) {
-  const [renaming, setRenaming] = useState(false);
-  const [newName, setNewName] = useState('');
-  const context = useInteractiveConfig();
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  const { data: chainData, error } = useChain(searchParams.get('chain') ?? undefined);
-
-  useEffect(() => {
-    if (renaming) {
-      setNewName(searchParams.get('chain') ?? '');
-    }
-  }, [renaming]);
-
-  const handleDelete = async () => {
-    await context.sdk.deleteChain(searchParams.get('chain') ?? '');
-    router.push(pathname);
-  };
-
-  const handleRename = async () => {
-    if ((newName && newName !== searchParams.get('chain')) ?? '') {
-      await context.sdk.renameChain(searchParams.get('chain') ?? '', newName);
-      setRenaming(false);
-      const current = new URLSearchParams(Array.from(searchParams.entries()));
-      current.set('chain', newName);
-      router.push(`${pathname}?${current.toString()}`);
-    }
-  };
-
-  const handleExportChain = async () => {
-    const chainData = await context.sdk.getChain(searchParams.get('chain') ?? '');
-    const element = document.createElement('a');
-    const file = new Blob([JSON.stringify(chainData.steps)], { type: 'application/json' });
-    element.href = URL.createObjectURL(file);
-    element.download = `${searchParams.get('chain') ?? ''}.json`;
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-  };
-
+export default function ChainPanel() {
   return (
-    <div className='space-y-4'>
+    <div>
       <SidebarGroup>
         <SidebarGroupLabel>Select Chain</SidebarGroupLabel>
         <SidebarMenuButton className='group-data-[state=expanded]:hidden'>
           <ArrowBigLeft />
         </SidebarMenuButton>
         <div className='w-full group-data-[collapsible=icon]:hidden'>
-          {renaming ? (
-            <Input value={newName} onChange={(e) => setNewName(e.target.value)} className='w-full' />
-          ) : (
-            <ChainSelector />
-          )}
+          <ChainSelector />
         </div>
+      </SidebarGroup>
+      <SidebarGroup>
         <SidebarGroupLabel>Chain Functions</SidebarGroupLabel>
         <SidebarMenu>
-          {[
-            {
-              title: 'Create Chain',
-              icon: Plus,
-              func: () => setShowCreateDialog(true),
-              disabled: renaming || showCreateDialog,
-            },
-            {
-              title: renaming ? 'Save Name' : 'Rename Chain',
-              icon: renaming ? Check : Pencil,
-              func: renaming ? handleRename : () => setRenaming(true),
-              disabled: !chainData || (renaming && (!newName || newName === searchParams.get('chain'))),
-            },
-            {
-              title: 'Export Chain',
-              icon: Download,
-              func: handleExportChain,
-              disabled: !chainData || renaming,
-            },
-            {
-              title: 'Delete Chain',
-              icon: Trash2,
-              func: handleDelete,
-              disabled: !chainData || renaming,
-            },
-          ].map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton side='left' tooltip={item.title} onClick={item.func} disabled={item.disabled}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
           <ChainCreate />
           <ChainRename />
           <ChainExport />
